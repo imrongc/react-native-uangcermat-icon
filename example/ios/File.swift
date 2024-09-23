91b518a78db0dd3386cf273b5aff3df8e02107f2
imrongc <imron@gajicermat.com>
2024-09-23 09:07:46 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  UangcermatIconExample
+//
+
+import Foundation
---
+++ ios/UangcermatIcon.swift
@@ -0,0 +1,8 @@
+@objc(UangcermatIcon)
+class UangcermatIcon: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
