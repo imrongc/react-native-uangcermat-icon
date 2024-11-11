b5cf8fe4595de2bb90ae80d90ca3c87437c59e15
imrongc <imron@gajicermat.com>
2024-11-11 13:05:32 +0700

    chore: fixing missing export

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,2 +1,8 @@
 export { Icon } from './Icon';
 export type { IconProps, IconType } from './Icon.type';
+export {
+  DuotoneIconArr,
+  FillIconArr,
+  OutlineIconArr,
+  IconArr,
+} from './Icon.type';
